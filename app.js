a62c6dc1c77a23fb7b78b40c08bc1a47aeb7b724
jlpg81 <jlpg81@gmail.com>
2019-03-29 01:46:25 +0100

    backup before merging with nodemailer

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const   express                 = require('express'),
         methodOverride          = require('method-override'),
         User                    = require('./models/user'),
         Post                    = require('./models/post'),
-        Comment                 = require('./models/comment');
+        Comment                 = require('./models/comment'),
+        nodemailer              = require('nodemailer');
 
 var PORT = 3002;
 
@@ -30,6 +31,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
 //Other setup
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
@@ -50,33 +52,92 @@ app.get('/register', function(req, res){
     res.render('register');
 });
 
-app.post('/register', function(req, res){
-    User.register(new User({
-        username: req.body.username,
-        nickname: req.body.nickname, 
-        firstName: req.body.firstName, 
-        lastName: req.body.lastName,
-        country: req.body.country,
-        state: req.body.state,
-        nacimiento: req.body.nacimiento,
-        profesion: req.body.profesion,
-        telefono: req.body.telefono,
-        redSocial1: req.body.redSocial1,
-        redSocial2: req.body.redSocial2,
-        cedula: req.body.cedula,
-        ideologia: req.body.ideologia,
-        otras: req.body.otras,
-        }), 
-        req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render('register');
-        }
-        passport.authenticate('local')(req,res, function(){
-            res.redirect('/success');
-        })
+// Nodemailer Setup
+app.post('/register', (req, res) => {
+    const output = `
+      <h3>Un nuevo miembro se ha inscrito en el MLV:</h3>
+      <ul>  
+        <li>Name: ${req.body.username}</li>
+        <li>Company: ${req.body.nickname}</li>
+        <li>Email: ${req.body.firstName}</li>
+        <li>Phone: ${req.body.lastName}</li>
+        <li>username: ${req.body.username}</li>
+        <li>nickname: ${req.body.nickname}</li> 
+        <li>firstName: ${req.body.firstName}</li>
+        <li>lastName: ${req.body.lastName}</li>
+        <li>country: ${req.body.country}</li>
+        <li>state: ${req.body.state}</li>
+        <li>nacimiento: ${req.body.nacimiento}</li>
+        <li>profesion: ${req.body.profesion}</li>
+        <li>telefono: ${req.body.telefono}</li>
+        <li>redSocial1: ${req.body.redSocial1}</li>
+        <li>redSocial2: ${req.body.redSocial2}</li>
+        <li>cedula: ${req.body.cedula}</li>
+        <li>ideologia: ${req.body.ideologia}</li>
+        <li>otras: ${req.body.otras}</li>
+      </ul>
+    `;
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.googlemail.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+          user: 'jlpg81@gmail.com', // generated ethereal user
+          pass: 'DamnVenezuela'  // generated ethereal password
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
     });
-});
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <jlpg81@gmail.com>', // sender address
+        to: 'jlpg81@gmail.com', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+        res.render('success');
+    });
+    });
+
+
+// app.post('/register', function(req, res){
+//     User.register(new User({
+//         username: req.body.username,
+//         nickname: req.body.nickname, 
+//         firstName: req.body.firstName, 
+//         lastName: req.body.lastName,
+//         country: req.body.country,
+//         state: req.body.state,
+//         nacimiento: req.body.nacimiento,
+//         profesion: req.body.profesion,
+//         telefono: req.body.telefono,
+//         redSocial1: req.body.redSocial1,
+//         redSocial2: req.body.redSocial2,
+//         cedula: req.body.cedula,
+//         ideologia: req.body.ideologia,
+//         otras: req.body.otras,
+//         }), 
+//         req.body.password, function(err, user){
+//         if(err){
+//             console.log(err);
+//             return res.render('register');
+//         }
+//         passport.authenticate('local')(req,res, function(){
+//             res.redirect('/success');
+//         })
+//     });
+// });
 
 app.get('/success', function(req, res){
     res.render('success');
